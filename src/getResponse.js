0bc11927cba320b1cac4ffb5d62d09c40870c460
masha yanson <mashayanson@gmail.com>
2024-02-01 13:25:56 +0100

    disableCache=true

--- src/getResponse.js
+++ src/getResponse.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import parse from "./parse.js";
 
 const getResponse = (link) => {
-    const url = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(link)}`
+    const url = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(link)}`
     return axios.get(url, { timeout: 10000 }).then((resp)=>{
        return parse(resp, link)
      }).catch((e)=>{
