203d7c01b924a0ad4ea29ae46041aa166dd2abb5
masha yanson <mashayanson@gmail.com>
2024-02-14 11:46:01 +0100

    .then block changed

--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ import validation from './validationSchema.js';
 import updateFeed from './updatePosts.js';
 
 export default function App() {
+  const state = {
+    validation: true,
+    feeds: [],
+    posts: [],
+    readedPosts: [],
+    loadingStatus: null,
+    textError: '',
+    isError: false,
+    modalIndex: null,
+
+  };
+
   const i18Instance = i18n.createInstance();
   i18Instance.init({
     lng: 'ru',
@@ -31,76 +43,63 @@ export default function App() {
         notOneOf: 'errors.rssExists',
       },
     });
-  });
-  const state = {
-    validation: true,
-    feeds: [],
-    posts: [],
-    readedPosts: [],
-    loadingStatus: null,
-    textError: '',
-    isError: false,
-    modalIndex: null,
+    // eslint-disable-next-line no-use-before-define
+    const watchedState = onChange(state, handleRender);
+    const form = document.getElementById('urlform');
 
-  };
+    function refreshFeeds() {
+      setTimeout(function repeat() {
+        const promises = watchedState.feeds.map((feed) => getResponse(feed.feedLink));
+        Promise.all(promises).then((responses) => {
+          responses.forEach((feed) => {
+            updateFeed(watchedState, feed);
+          });
+        }).catch();
+        setTimeout(repeat, 5000);
+      }, 5000);
+    }
 
-  // eslint-disable-next-line no-use-before-define
-  const watchedState = onChange(state, handleRender);
-  const form = document.getElementById('urlform');
-
-  function refreshFeeds() {
-    setTimeout(function repeat() {
-      const promises = watchedState.feeds.map((feed) => getResponse(feed.feedLink));
-      Promise.all(promises).then((responses) => {
-        responses.forEach((feed) => {
+    function handleSubmit(event) {
+      event.preventDefault();
+      const urlInput = document.getElementById('inputAddress');
+      const urlValue = urlInput.value;
+      const links = watchedState.feeds.map(({ feedLink }) => feedLink);
+      validation(urlValue, links, i18Instance)
+        .then((value) => {
+          watchedState.loadingStatus = 'loading';
+          return getResponse(value, watchedState, i18Instance);
+        })
+        .then((feed) => {
+          watchedState.loadingStatus = 'success';
+          watchedState.textError = 'interface.loadSuccess';
+          if (!watchedState.feeds.length) {
+            refreshFeeds();
+          }
           updateFeed(watchedState, feed);
+        })
+        .catch((error) => {
+          watchedState.isError = true;
+          watchedState.loadingStatus = 'failed';
+          watchedState.textError = error.message;
+          watchedState.validation = false;
         });
-      }).catch();
-      setTimeout(repeat, 5000);
-    }, 5000);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const urlInput = document.getElementById('inputAddress');
-    const urlValue = urlInput.value;
-    const links = watchedState.feeds.map(({ feedLink }) => feedLink);
-    validation(urlValue, links, i18Instance)
-      .then((value) => {
-        watchedState.loadingStatus = 'loading';
-        return getResponse(value, watchedState, i18Instance);
-      })
-      .then((feed) => {
-        watchedState.loadingStatus = 'success';
-        watchedState.textError = 'interface.loadSuccess';
-        if (!watchedState.feeds.length) {
-          refreshFeeds();
-        }
-        updateFeed(watchedState, feed);
-      })
-      .catch((error) => {
-        watchedState.isError = true;
-        watchedState.loadingStatus = 'failed';
-        watchedState.textError = error.message;
-        watchedState.validation = false;
-      });
-  }
-
-  form.addEventListener('submit', handleSubmit);
-
-  const posts = document.getElementById('content');
-  posts.addEventListener('click', (e) => {
-    const readPost = watchedState.readedPosts;
-    if (e.target.dataset.readedLink && !readPost.includes(e.target.dataset.readedLink)) {
-      watchedState.readedPosts.push(e.target.dataset.readedLink);
     }
-    if (e.target.dataset.modalIndex) {
-      watchedState.modalIndex = e.target.dataset.modalIndex;
+
+    form.addEventListener('submit', handleSubmit);
+
+    const posts = document.getElementById('content');
+    posts.addEventListener('click', (e) => {
+      const readPost = watchedState.readedPosts;
+      if (e.target.dataset.readedLink && !readPost.includes(e.target.dataset.readedLink)) {
+        watchedState.readedPosts.push(e.target.dataset.readedLink);
+      }
+      if (e.target.dataset.modalIndex) {
+        watchedState.modalIndex = e.target.dataset.modalIndex;
+      }
+    });
+
+    function handleRender() {
+      render(watchedState, i18Instance);
     }
   });
-
-  function handleRender() {
-    render(watchedState, i18Instance);
-  }
 }
